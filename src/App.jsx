bdc4806b4c5f1ba3503f2aad28b04bd641fc28ac
muzammilbinmarsuhen <j7958962@gmail.com>
2024-07-05 21:36:20 +0700

    Edit App.jsx dan android terbaru

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,82 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from './screens/Home';
+import Menu from './screens/Menu';
+import Cart from './screens/Cart';
+import Checkout from './screens/Checkout';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const MenuTab = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <Tabs.Navigator screenOptions={{tabBarActiveTintColor: '#eddcd2'}}>
+      <Tabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Menu"
+        component={Menu}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons name="menu" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons
+              name="cart-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Checkout"
+        component={Checkout}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons
+              name="cart-check"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={MenuTab}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
