1e5de963a3c6ed2cbe4645f3741717e8f49047fa
muzammilbinmarsuhen <j7958962@gmail.com>
2024-07-07 00:50:46 +0700

    mengubah tampilan splash

--- src/screens/Splash.jsx
+++ src/screens/Splash.jsx
@@ -1,32 +1,36 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, Text, View, Image, StatusBar} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  ActivityIndicator,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 const Splash = () => {
   const navigation = useNavigation();
 
   useEffect(() => {
-    
     const delay = setTimeout(() => {
-      navigation.replace('Tab'); 
-    }, 2000); 
+      navigation.replace('Tab');
+    }, 2000);
 
-   
     StatusBar.setHidden(true);
 
-   
     return () => clearTimeout(delay);
   }, []);
 
   return (
     <View style={styles.container}>
-      {/* Tambahkan gambar atau logo di bawah ini */}
       <Image
         style={styles.logo}
         source={require('../assets/Splash/babe.png')}
         resizeMode="contain"
       />
-      <Text style={styles.text}></Text>
+      <Text style={styles.text}>Sabar Ini Ujian</Text>
+      <ActivityIndicator size="large" color="#000" style={{marginTop: 16}} />
     </View>
   );
 };
@@ -34,20 +38,21 @@ const Splash = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FF6347', // Ganti dengan warna latar belakang yang diinginkan
+    backgroundColor: '#FF6347',
     justifyContent: 'center',
     alignItems: 'center',
   },
   logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 20, // Sesuaikan jaraknya sesuai keinginan
+    width: 300,
+    height: 300,
+    marginBottom: 20,
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#333333', // Sesuaikan warna teks
+    color: '#000',
+    fontFamily: 'Lato',
   },
 });
 
-export default Splash; // Pastikan diekspor secara default
+export default Splash;
